83b120c16fea58a5c2b7b7102c6659786341f625
joaquineichholz <jeichholz3@uc.cl>
2020-03-31 00:50:39 +0100

    apisode table

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    episodes_list = [[1, 2, 3, 4], [1, 3, 4, 5], [5, 5, 6, 7]]
+
+    return render_template('index.html', list=episodes_list)
 
 if __name__ == '__main__':
     app.run()
