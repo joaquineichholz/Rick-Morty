f6acd97f2ff8c0cff50044aae7ca4303307ebbd6
joaquineichholz <jeichholz3@uc.cl>
2020-03-31 02:18:31 +0100

    apisode table

--- app.py
+++ app.py
@@ -4,10 +4,30 @@ import requests
 
 app = Flask(__name__)
 
+def capitulos():
+    response = requests.get('https://rickandmortyapi.com/api/episode/')
+    pages = response.json()['info']['pages']
+    info = []
+    first = True
+
+    for pag in range(1, pages + 1):
+        if not first:
+            response = requests.get('https://rickandmortyapi.com/api/episode/?page=' + str(pag))
+
+        for episode in response.json()['results']:
+            info.append([episode['id'], episode['name'], episode['air_date'], episode['episode'], episode['url']])
+
+            print(episode['id'], episode['name'])
+
+        first = False
+
+    return info
+
+
 
 @app.route('/')
 def hello_world():
-    episodes_list = [[1, 2, 3, 4], [1, 3, 4, 5], [5, 5, 6, 7]]
+    episodes_list = capitulos()
 
     return render_template('index.html', list=episodes_list)
 
